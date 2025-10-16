6436d5e9d049f519eeab1878d671149701bf2a50
mintRaven <debjeetbanerjee48@gmail.com>
2025-10-16 08:44:52 +0530

    [FIX-LINK]: fix linkedin profile link

--- app/page.jsx
+++ app/page.jsx
@@ -13,7 +13,7 @@ import CountUp from "react-countup";
 
 const socials = [
   { icon: <FaGithub />, path: "https://github.com/mintRaven-05"},
-  { icon: <FaLinkedin />, path: "https://www.linkedin.com/in/~debjeetbanerjee/"},
+  { icon: <FaLinkedin />, path: "https://www.linkedin.com/in/debjeetbanerjee48/"},
   { icon: <FaXTwitter />, path: "https://x.com/mintRaven_05"},
   {icon: <MdAttachEmail />, path: "mailto:debjeet.banerjee2023@uem.edu.in"},
 ];
